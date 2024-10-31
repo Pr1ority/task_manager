5ac619ddbf5dce17b13ba2a77d36a2413b3a4014
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-11-01 00:19:20 +0300

    new branch

---
+++ Jenkinsfile
@@ -0,0 +1,28 @@
+pipeline {
+
+    agent none
+    stages {
+        stage('Build') {
+            agent {
+                docker {
+                    image 'python:3.10-slim'
+                    args '-u root'
+                }
+            }
+            steps {
+                sh 'pip install -r requirements.txt'
+                sh 'python manage.py collectstatic --noinput'
+
+            }
+        }
+        stage('Deploy'){
+            agent any
+            steps {
+                script {
+                    sh 'docker compose down'
+                    sh 'docker compose up -d --build'
+                }
+            }
+        }
+    }
+}
---
+++ tasks/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from .constants import TITLE_MAX_LENGTH, STATUS_MAX_LENGTH
+
+class Task(models.Model):
+    TODO = 'to do'
+    IN_PROGRESS = 'in progress'
+    DONE = 'done'
+    STATUS = [
+        (TODO, 'To Do'),
+        (IN_PROGRESS, 'In Progress'),
+        (DONE, 'Done')
+        ]
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=STATUS_MAX_LENGTH, choices=STATUS,
+                              default=TODO)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.title
